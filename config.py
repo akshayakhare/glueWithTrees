2bebf445268d76d43bfce9c87a9598ab209f3c03
akshayakhare <akshayakhare@gmail.com>
2017-12-05 00:42:13 +0000

    glue config file added

---
+++ config.py
@@ -0,0 +1,17 @@
+# from glue.qt.widgets.data_viewer import DataViewer
+
+from MyWidget import ApplicationWindow
+
+
+# class MyGlueWidget(DataViewer):
+class MyGlueWidget(ApplicationWindow):
+
+    def __init__(self, session, parent=None):
+        super(MyGlueWidget, self).__init__(session, parent=parent)
+        self.my_widget = MyWidget()
+        self.setCentralWidget(self.my_widget)
+
+
+# Register the viewer with glue
+from glue.config import qt_client
+qt_client.add(MyGlueWidget)
