678d462a623c9797ec4d666226445eee78466cc2
akshayakhare <akshayakhare@gmail.com>
2017-12-14 22:26:20 +0000

    workign outside glue

--- config.py
+++ config.py
@@ -1,51 +1,29 @@
-# from glue.qt.widgets.data_viewer import DataViewer
 from glue.viewers.common.qt.data_viewer import DataViewer
+import matplotlib.pyplot as plt
 
-# from MyWidget import MyWidget
-# from Document import Window
-from circularTree import CircularTree
+# from circularTree import CircularTree
+from document4 import Window
 import sys
 import networkx as nx
 from PyQt4 import QtGui, QtCore
 
-# try:
-#     import pygraphviz
-#     from networkx.drawing.nx_agraph import graphviz_layout
-# except ImportError:
-#     try:
-#         import pydot
-#         from networkx.drawing.nx_pydot import graphviz_layout
-#     except ImportError:
-#         raise ImportError("This example needs Graphviz and either "
-#                           "PyGraphviz or pydot")
-
 
 class MyGlueWidget(DataViewer):
-    # class MyGlueWidget(ApplicationWindow):
-
     LABEL = "My first data viewer"
 
     def __init__(self, session, parent=None):
         super(MyGlueWidget, self).__init__(session, parent=parent)
-        # G = nx.balanced_tree(3, 5)
-        qApp = QtGui.QApplication(sys.argv)
-        # qApp.exec_()
-        # self.my_widget = MyWidget()
-        self.my_widget = CircularTree(QtGui.QWidget())
-        # sys.exit(qApp.exec_())
-
-        self.setCentralWidget(self.my_widget)
+        self.my_widget = Window()
+        self.my_widget.show()
+        # self.setCentralWidget(self.my_widget)
         # self.add_data()
-        # sys.exit(qApp.exec_())
 
     def add_data(self):
         print("here?\n")
-        self.my_widget.setWindowTitle("MyWindowMyRules")
-        # self.my_widget.plt2.show()
+        self.my_widget.setWindowTitle("MyWindow")
+        # self.my_widget.dummyFunc()
+        self.my_widget.show()
         print("here????\n")
-        # qApp.exec_()
-
-        # sys.exit(qApp.exec_())
         return True
 
 
